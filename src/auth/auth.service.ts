7b9d06fc9b7a539a98cb4d96d175e9c5cc39f526
Jiyun Kim <wayandway@kakao.com>
2024-12-15 03:57:57 +0900

    fix: 쓰지 않는 NotFoundException import 제거

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../user/schemas/user.schema';
